a688fb391c448051dfc79bc434df885886ed9646
alinabodnar1 <malina.bodnar@gmail.com>
2023-06-10 14:44:00 +0300

    Update ContactsList.jsx

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -6,7 +6,7 @@ import { useSelector } from 'react-redux';
 export default function ContactsList() {
     const contacts = useSelector(state => state.contacts);
     const currentFilter = useSelector(state => state.filter);
-    const visibleContacts = (contacts, currentFilter) => {
+    const visibleContacts = () => {
          return contacts.filter(contact => contact.toLowerCase().trim().includes(currentFilter));
     }
     return (<ul>
@@ -19,3 +19,4 @@ export default function ContactsList() {
         </ul>
     )
 }
+
