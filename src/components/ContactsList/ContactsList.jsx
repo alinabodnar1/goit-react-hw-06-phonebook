b7e687c943a995053d0c51453db78689368fd5fe
alinabodnar1 <malina.bodnar@gmail.com>
2023-06-10 11:05:33 +0300

    fixed visibleContacts

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -2,16 +2,15 @@ import React from 'react';
 import ContactsListItem from 'components/ContactsListItem/ContactsListItem';
 import { useSelector } from 'react-redux';
 
+
 export default function ContactsList() {
     const contacts = useSelector(state => state.contacts);
     const currentFilter = useSelector(state => state.filter);
-    
     const visibleContacts = (contacts, currentFilter) => {
          return contacts.filter(contact => contact.toLowerCase().trim().includes(currentFilter));
     }
-  
     return (<ul>
-            { visibleContacts.map(contact => (
+            { visibleContacts().map(contact => (
                 <ContactsListItem
                     key={contact.id}
                     contact={contact}
